7e7e8f6e513b1fbc19e25335f443076a41009478
angelaolinda <olinda.angela@aluno.ufca.edu.br>
2025-07-06 22:18:26 -0300

    Create q11.py

---
+++ q11.py
@@ -0,0 +1,9 @@
+pessoas = int(input("Quantidade de pessoas: "))
+carne_por_p = 0.5 
+cerveja_por_p = 6
+valor_cerveja = 1.70
+valor_carne = 18.0
+total_cerveja = pessoas* valor_cerveja* cerveja_por_p 
+total_carne = pessoas* valor_carne* carne_por_p
+print ("O valor total do custo de carne é R$", total_carne )
+print ("E o valor da cerveja é R$", total_cerveja)
