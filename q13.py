5c286fb04b459abc9e8d6620f9ed73e103b73670
angelaolinda <olinda.angela@aluno.ufca.edu.br>
2025-07-06 22:38:43 -0300

    Create q13.py

---
+++ q13.py
@@ -0,0 +1,7 @@
+qrosas = int(input("Quantidade de rosas: "))
+qtulipas = int(input("Quantidade de tulipas: "))
+valorr = 2.80
+valort = 4.20
+totalr = qrosas* valorr
+totalt = qtulipas* valort
+print ("O valor para", qrosas, "rosas é R$", totalr, "e para", qtulipas, "tulipas é R$", totalt)
