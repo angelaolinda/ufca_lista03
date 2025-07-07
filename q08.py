0af50eff1ffa51cd0da10837e081fa85ae701df8
angelaolinda <olinda.angela@aluno.ufca.edu.br>
2025-07-06 21:39:00 -0300

    Create q08.py

---
+++ q08.py
@@ -0,0 +1,6 @@
+print("Calcuradora de velocidade\n")
+piloto = input("Piloto: ")
+distancia = int(input("Distância (Km): "))
+tempo = int(input("Tempo (h): "))
+velocidade = distancia/ tempo
+print ("A velocidade média do", piloto, "foi de", velocidade, "Km/h")
